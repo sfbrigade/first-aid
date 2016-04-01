ce2f2bcb17945c1404d960844283f22975a35051
Jeremy Oltean & Matt Harris <pair+goldtean.matrharr@devbootcamp.com>
2016-04-01 16:34:21 -0700

    add basic routes/views/controllers

---
+++ app/controllers/charities_controller.rb
@@ -0,0 +1,7 @@
+class CharitiesController < ApplicationController
+
+  def show
+    #a specific charity's page w/info
+  end
+
+end
---
+++ app/controllers/disasters_controller.rb
@@ -0,0 +1,20 @@
+class DisastersController < ApplicationController
+
+  def map
+    #first page, narrow feed to map selection
+  end
+
+  def index
+    #home page, feed of all disasters in narrowed map scope
+    @disasters = Disaster.all
+  end
+
+  def show
+    #a specific disaster with associated charities
+    @disaster = Disaster.find(params[:id])
+    #list nearby charities
+    # @charities = Charities.find_by()
+  end
+
+
+end
