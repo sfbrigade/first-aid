c5794849ae8c4e3aae01cbc8714a8b5e94052c93
Matt Harris <matrharr@gmail.com>
2016-04-03 18:27:32 -0700

    add js for variable donation amount

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,4 @@
 //= require bootstrap
 //= require turbolinks
 //= require map
+//= require stripe
--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -13,24 +13,27 @@ class CharitiesController < ApplicationController
 
   # POST from charity donation form
   def stripe_charge
+
+    donation = Donation.create(user_id: current_user.id, disaster_id: params[:disaster_id], charity_id: params[:id], amount: params[:donation_amount])
+
     Stripe.api_key = "sk_test_8zheMKz7JDMHp5rgwd5yR1ly"
-    # Get the credit card details submitted by the form
+    p params
     token = params[:stripeToken]
-    # Create a Customer
-    customer = Stripe::Customer.create(
-      :source => token,
-      :description => "Example customer"
-    )
-    # Charge the Customer instead of the card
+
+      customer = Stripe::Customer.create(
+        :source => token,
+        :description => "Example customer"
+      )
+    # current_user.token = customer //save user token
     begin
       charge = Stripe::Charge.create(
-        :amount => 1000, # in cents
+        :amount => donation.amount, # in cents
         :currency => "usd",
         :customer => customer.id
       )
-      flash[:notice] = "Thanks for donating!"
+      flash[:good] = "Thanks for donating!"
     rescue Stripe::CardError => e
-      flash[:danger] = e.message
+      flash[:problem] = e.message
     end
     redirect_to "/disasters/#{params[:disaster_id]}/charities/#{params[:id]}"
   end
