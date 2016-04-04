77903beb83badf90f80f27b602612132fb2f42a6
Todd Seller <me@toddseller.com>
2016-04-04 09:42:57 -0700

    Add background logic to project

--- app/models/disaster.rb
+++ app/models/disaster.rb
@@ -3,22 +3,19 @@ class Disaster < ActiveRecord::Base
   has_many :users, through: :donations
   has_many :charities, through: :donations
 
-  before_create :new_disaster
+  # before_create :new_disaster
 
+  def new_disaster
+    lat = []
+    long = []
+    uri = URI("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson")
+    @location = Net::HTTP.get_response(uri).body
 
-  private
-
-    def new_disaster
-      lat = []
-      long = []
-      uri = URI("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson")
-      @location = Net::HTTP.get_response(uri).body
-
-      json = JSON.parse(@location)
-      json['features'].each do |locations|
-         self.longitude = locations['geometry']['coordinates'][0]
-         self.latitude = locations['geometry']['coordinates'][1]
-      end
-
+    json = JSON.parse(@location)
+    json['features'].each do |locations|
+       self.longitude = locations['geometry']['coordinates'][0]
+       self.latitude = locations['geometry']['coordinates'][1]
     end
+
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,6 +48,8 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.gem 'whenever', :lib => false, :source => 'http://gems.github.com'
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
