c7151bda77148c17eefb79509a4e5ee94dfa1124
Cody <email": null>
2016-04-04 11:35:04 -0700

    add tests

---
+++ test/models/disaster_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class DisasterTest < ActiveSupport::TestCase
+    it 'create creates a new disaster' do
+    before_count = Disaster.count
+    Disaster.create(latitude: 44, longitude: 123, category: "earthquake")
+    expect(Disaster.count).to_not eq(before_count)
+  end
+end
